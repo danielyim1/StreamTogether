009487a9a77e0d923e4cf8e1672752bc6f39e692
Daniel Yim <dyim2@jhu.edu>
2026-01-04 16:30:45 -0500

    Initial commit with Flask app and session page

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, leave_room
-import random
-import string
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "change-me-in-production"
@@ -16,36 +14,54 @@ def home():
 def session():
     return render_template("session.html")
 
+# ---------------- Socket.IO Handlers ---------------- #
+
 @socketio.on('join')
-def handle_join(sessionId, clientId):
+def handle_join(data):
+    sessionId = data['sessionId']
+    clientId = data['clientId']
     join_room(sessionId)
     socketio.emit('join', clientId, room=sessionId, include_self=False)
 
 @socketio.on('leave')
-def handle_leave(sessionId, clientId):
+def handle_leave(data):
+    sessionId = data['sessionId']
+    clientId = data['clientId']
     leave_room(sessionId)
     socketio.emit('leave', clientId, room=sessionId, include_self=False)
 
 @socketio.on('change_video')
-def handle_change_video(sessionId, isPlaying, currentTime):
+def handle_change_video(data):
+    sessionId = data['sessionId']
+    isPlaying = data['isPlaying']
+    currentTime = data['currentTime']
     socketio.emit(
         'change_video_state',
-        data=(isPlaying, currentTime),
+        {'isPlaying': isPlaying, 'currentTime': currentTime},
         room=sessionId
     )
 
 @socketio.on('set_video')
-def handle_set_video(sessionId, id):
-    socketio.emit('upload_video', id, room=sessionId)
+def handle_set_video(data):
+    sessionId = data['sessionId']
+    video_id = data['id']
+    socketio.emit('upload_video', video_id, room=sessionId)
 
 @socketio.on('send_heartbeat')
-def handle_heartbeat(sessionId, id, currentTime, isPlaying, joinedNames):
+def handle_heartbeat(data):
+    sessionId = data['sessionId']
+    video_id = data['id']
+    currentTime = data['currentTime']
+    isPlaying = data['isPlaying']
+    joinedNames = data['joinedNames']
     socketio.emit(
         'get_heartbeat',
-        data=(id, currentTime, isPlaying, joinedNames),
+        {'id': video_id, 'currentTime': currentTime, 'isPlaying': isPlaying, 'joinedNames': joinedNames},
         room=sessionId,
         include_self=False
     )
+
+# ---------------- Run App ---------------- #
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
